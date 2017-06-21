6d441e333a660943236567b1388d758f54e836be
Hsu-Chen <102703023@nccu.edu.tw>
2017-06-21 16:26:24 +0800

    change head_img button

--- firebase_example/personalpage_new.js
+++ firebase_example/personalpage_new.js
@@ -15,14 +15,22 @@ firebase.auth().onAuthStateChanged(function(user) {
         result.push(snapshot.val());
 
 
-    });
+        });
     
-    var article = new Vue({
-        el: '#article',
-        data: {
-            questionCards: result
-        }
-        })
+        var article = new Vue({
+            el: '#article',
+            data: {
+                questionCards: result
+            },
+            methods:{
+                cancel: function(){
+                    //alert(this.id);
+                    firebase.database().ref('/posts/'+ this.id).remove().then(function(){
+                        window.location.href = 'personalPage.html';
+                    });
+                }
+            }
+        });
     }
 
 });
